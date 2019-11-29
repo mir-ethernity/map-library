77e0951eadc62d806a26448db095bfe62bcdb7a1
bvadell <bvadell@intricom.es>
2019-11-29 09:50:27 +0100

    Generate class with map definition

---
+++ src/Mir.Ethernity.MapLibrary/Mir.Ethernity.MapLibrary/Map.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mir.Ethernity.MapLibrary
+{
+    public class Map
+    {
+        public ushort Width { get; set; }
+        public ushort Height { get; set; }
+        public MapCell[] Cells { get; set; }
+    }
+}
---
+++ src/Mir.Ethernity.MapLibrary/Mir.Ethernity.MapLibrary/MapCell.cs
@@ -0,0 +1,19 @@
+﻿namespace Mir.Ethernity.MapLibrary
+{
+    public class MapCell
+    {
+        public byte BackFile { get; set; }
+        public ushort BackImage { get; set; }
+
+        public byte FrontAnimationFrame { get; set; }
+        public byte FrontFile { get; set; }
+        public ushort FrontImage { get; set; }
+
+        public byte MiddleAnimationFrame { get; set; }
+        public byte MiddleFile { get; set; }
+        public ushort MiddleImage { get; set; }
+
+        public byte Light { get; set; }
+        public bool Flag { get; set; }
+    }
+}
